e933645db1966c64c3cabe1023cf08a1b7a62c3a
FelipeKonig <lipekonig@gmail.com>
2020-04-17 23:44:41 -0300

    My blog with custom page 404

---
+++ mysite/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '51l$(*ihem^3)9v8jf^sk6k!lq^5ep7v3dzq4!o11x$^dqbqu&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+DB_PASSWORD = 'Fk10125503108'
+DB_USER = 'bloguser'
+DB_NAME = 'blog'
+DB_HOST = 'localhost'
